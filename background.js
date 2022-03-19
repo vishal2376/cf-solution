0fa5ac5645605314371d3ad484fc2ec4bbe06705
vishal2376 <vishalsingh2376@gmail.com>
2022-03-19 13:18:16 +0530

    feature: can open cf contest solutions

--- background.js
+++ background.js
@@ -3,5 +3,10 @@ chrome.action.onClicked.addListener((tab) => {
 	const cur_url = tab.url.split("/");
 	let sol_url = "https://codeforces.com/problemset/status/"+cur_url[5]+"/problem/"+cur_url[6];
     chrome.tabs.create({url: sol_url});
+} 
+else if(tab.url.includes("https://codeforces.com/contest/")) {
+	const cur_url = tab.url.split("/");
+	let sol_url = "https://codeforces.com/contest/"+cur_url[4]+"/status/"+cur_url[6];
+    chrome.tabs.create({url: sol_url});
 }
 });
